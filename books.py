ebee298cdeb4c388299bb7c784e293be14d6c75a
chip-byter <rizelleanndelacruz@gmail.com>
2025-05-08 13:04:44 +0800

    Add fallback for book covers

--- books.py
+++ books.py
@@ -1,3 +1,4 @@
+import os
 import customtkinter as ctk
 from database import Database
 from PIL import Image
@@ -34,6 +35,9 @@ class Books(ctk.CTkFrame):
 
         self.results_frame = ctk.CTkScrollableFrame(self)
         self.results_frame.grid(row=1, column=0, pady=(10, 0), sticky="nsew")
+
+        self.no_result = ctk.CTkFrame(self)
+        self.no_result.grid(row=1, column=0, pady=(10, 0), sticky="nsew")
         
 
     def search_query(self, q: str, target_frame):
@@ -43,13 +47,14 @@ class Books(ctk.CTkFrame):
         for widget in target_frame.winfo_children():
             widget.destroy()
 
-        self.db.cursor.execute("SELECT title, author FROM books_table WHERE title LIKE ? OR author LIKE ?", ("%" + q + "%", "%" + q + "%"))
+        self.db.cursor.execute("SELECT title, author, path FROM books_table WHERE title LIKE ? OR author LIKE ?", ("%" + q + "%", "%" + q + "%"))
         results = self.db.cursor.fetchall()
 
+        col = 0
+        row = 0
         if len(results) == 0:
             self.results_frame.grid_remove()
-            self.no_result = ctk.CTkFrame(self)
-            self.no_result.grid(row=1, column=0, pady=(10, 0), sticky="nsew")
+            self.no_result.grid()
             self.no_result.grid_columnconfigure(0, weight=1)
             self.no_result.grid_rowconfigure(0, weight=1)
             self.no_result.grid_rowconfigure(1, weight=1)
@@ -58,20 +63,17 @@ class Books(ctk.CTkFrame):
             self.message.grid(row=0, column=0, sticky="sew")
             self.sub_message = ctk.CTkLabel(self.no_result, text=f"Please check your spelling or enter a specific book title or author.", font=("Helvetica", 13, "italic"))
             self.sub_message.grid(row=1, column=0, sticky="new")
-            
         else:
-            target_frame.grid()
-            self.no_result.grid_remove()
-            col = 0
-            row = 0
-            
-            for title, author in results:
-                self.book_card = BookCard(target_frame, title, author, "images/the_catcher_in_the_rye_cover.jpg")
+            for title, author, path in results:
+                self.book_card = BookCard(target_frame, title, author, path)
                 self.book_card.grid(row=row, column=col, pady=(0, 10), padx=(0, 10), sticky="w")
                 col+= 1
                 if col >= 4:  
                     col = 0
                     row += 1
+            self.no_result.grid_remove()
+            target_frame.grid()
+        
 
 class BookCard(ctk.CTkFrame):
     def __init__(self, parent, book_title:str, author:str, path:str):
@@ -80,10 +82,19 @@ class BookCard(ctk.CTkFrame):
         self.title = book_title
         self.author = author
         self.path = path
+        self.default_image_path = "images/default_cover.png"
 
-        self.image = ctk.CTkImage(dark_image=Image.open(self.path), size=(145, 217))
-        self.book_cover = ctk.CTkLabel(self, text="", image=self.image)
-        self.book_cover.grid(row=0, column=0, padx=5, pady=5, sticky="nsew")
+        
+        # if os.path.exists(self.path):
+        try:    
+            self.image = ctk.CTkImage(dark_image=Image.open(self.path), size=(145, 217))
+            self.book_cover = ctk.CTkLabel(self, text="", image=self.image) 
+            self.book_cover.grid(row=0, column=0, padx=5, pady=5, sticky="nsew")
+
+        except FileNotFoundError:
+            self.default_image = ctk.CTkImage(dark_image=Image.open(self.default_image_path), size=(145, 217))
+            self.book_cover = ctk.CTkLabel(self, text="", image=self.default_image) 
+            self.book_cover.grid(row=0, column=0, padx=5, pady=5, sticky="nsew")
 
         self.book_metadata = ctk.CTkFrame(self, fg_color="transparent")
         self.book_metadata.grid(row=1, column=0, padx=5, pady=(0, 5), sticky="nsew")
@@ -101,5 +112,6 @@ class BookCard(ctk.CTkFrame):
         
 
 if __name__ == "__main__":
-    app = Books()
-    app.mainloop()
+    title = "The Little Prince".lower()
+    path = title.replace(" ", "_")
+    print(f"/images/{path}.jpg")
